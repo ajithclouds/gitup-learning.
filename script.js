320fd1a6292202117bd7ed54a1dd678cf884ac28
ajithclouds <ajithran9025@gmail.com>
2024-03-26 13:05:58 +0530

    counting programm

---
+++ script.js
@@ -0,0 +1,50 @@
+var span=document.getElementById("span");
+span.innerHTML=10
+// document.body.append(span)
+var count=10
+setTimeout(()=>{
+count=count-1
+span.innerHTML=count
+setTimeout(()=>{
+    count=count-1
+span.innerHTML=count
+setTimeout(()=>{
+    count=count-1
+    span.innerHTML=count
+    setTimeout(()=>{
+        count=count-1
+        span.innerHTML=count
+        setTimeout(()=>{
+            count=count-1
+            span.innerHTML=count
+            setTimeout(()=>{
+                count=count-1
+                span.innerHTML=count
+                setTimeout(()=>{
+                    count=count-1
+                    span.innerHTML=count
+                    setTimeout(()=>{
+                        count=count-1
+                        span.innerHTML=count
+                        setTimeout(()=>{
+                            count=count-1
+                            span.innerHTML=count
+                            setTimeout(()=>{
+                                count=count-1
+                                span.innerHTML="Happy Independance Day"
+                                
+                            },1000)  
+                        },1000)  
+                    },1000)
+                },1000)  
+            },1000)
+        },1000) 
+    },1000) 
+},1000)
+},1000)
+},1000)
+
+
+
+
+

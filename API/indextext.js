83cf9a9dfb902f96bad019bda8244d975477b60f
ajithclouds <ajithran9025@gmail.com>
2024-03-28 18:38:48 +0530

    sample .project

--- API/indextext.js
+++ API/indextext.js
@@ -1,9 +1,28 @@
-var res=fetch("https://restcountries.com/v3.1/all")
-.then((data)=>data.json()).then((data1)=>{
-    console.log(data1);
-    for(var i=0;i<data1.length;i++){
-        console.log(`Countryname:${data1[i].name.common} Capital:${data1[i].capital}
-        Flags:${data1[i].flags.png}
-        Time:${data1[i].timezones}`)
-    }
-})
+var res=fetch("https://restcountries.com/v3.1/all");
+res.then((data)=>data.json()).then((value)=>bar(value))
+var container=document.createElement("div")
+container.className="container"
+var row=document.createElement("div");
+row.className="row"
+container.append(row);
+
+function bar(value){
+    console.log(value);
+    for(var i=0;i<value.length;i++){
+    row.innerHTML+=`
+   <div class="col-md-4">
+    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+    <div class="card-header text-white bg-dark">${value[i].name.common}</div>
+    <div class="card-body">
+    <img src=${value[i].flags.png} class="card-img-top"  alt="...">
+      <p class="card-text">Capital:${value[i].capital}</p>
+      <p class="card-text">Region:${value[i].region}</p>
+      <p class="card-text">Latlong:${value[i].latlng}</p>
+ </div>
+ </div>
+ </div>
+
+    `
+}
+document.body.append(container)
+}

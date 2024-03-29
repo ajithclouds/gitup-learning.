4807163e10a5d52fd5e97af2dce80f063fbd5614
ajithclouds <ajithran9025@gmail.com>
2024-03-29 11:31:55 +0530

    project.4

--- API/indextext.js
+++ API/indextext.js
@@ -1,28 +1,28 @@
-var res=fetch("https://restcountries.com/v3.1/all");
+var res=fetch("https://jsonplaceholder.typicode.com/users ");
 res.then((data)=>data.json()).then((value)=>bar(value))
 var container=document.createElement("div")
 container.className="container"
 var row=document.createElement("div");
 row.className="row"
 container.append(row);
-
 function bar(value){
     console.log(value);
     for(var i=0;i<value.length;i++){
     row.innerHTML+=`
-   <div class="col-md-4">
-    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-    <div class="card-header text-white bg-dark">${value[i].name.common}</div>
-    <div class="card-body">
-    <img src=${value[i].flags.png} class="card-img-top"  alt="...">
-      <p class="card-text">Capital:${value[i].capital}</p>
-      <p class="card-text">Region:${value[i].region}</p>
-      <p class="card-text">Latlong:${value[i].latlng}</p>
+   <div class="col-md-3">
+   <div class="card"  style="width: 18rem; color:black ">
+   <div class="card-body">
+      <p class="card-text">Name:${value[i].name}</p>
+     <p class="card-text">Email:${value[i].email}</p>
+     <p class="card-text">Street:${value[i].address.street}</p>
+     <p class="card-text">City:${value[i].address.city}</p>
+    
+    
+   </div>
  </div>
- </div>
- </div>
-
+ 
     `
 }
 document.body.append(container)
+
 }
